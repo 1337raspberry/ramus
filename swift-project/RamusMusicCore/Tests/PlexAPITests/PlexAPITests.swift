22c2722c851afa886660d54285a469f44103d36a
raspberry1337 <admin@blueshelter.net>
2026-04-13 14:27:27 +0100

    remove historical swift stuff

--- swift-project/RamusMusicCore/Tests/PlexAPITests/PlexAPITests.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-@testable import PlexAPI
-
-final class PlexAPITests: XCTestCase {
-    func testPlexClientErrorCases() {
-        let error = PlexClientError.notConnected
-        XCTAssertTrue(error is PlexClientError)
-    }
-}
